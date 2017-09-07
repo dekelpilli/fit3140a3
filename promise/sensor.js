d540e1c7c2da3d343bda93fb145d76d328282a1f
dekelpilli <dekelpilli@gmail.com>
2017-09-07 18:39:39 +1000

    added some minor arduino code so that I can test it on my laptop

--- promise/sensor.js
+++ promise/sensor.js
@@ -6,3 +6,36 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://fit3140-a3.firebaseio.com"
 });
+
+var db = admin.database()
+var ref = db.ref('/motionCount')
+
+var five = require("johnny-five")
+
+board.on("ready", function () {
+  // interfaces for led and motion detector
+  var led = new five.Led(13)
+  var motion = new five.Motion(6)
+  // used to measure time
+  var startTime = null
+  var endTime = null
+
+  // turn LED on and measure starting time
+  motion.on("motionstart", function () {
+      led.on()
+      startTime = new Date()
+  })
+
+  // calculate time motion was detected using end time of motion
+  motion.on("motionend", function () {
+      led.off()
+      endTime = new Date()
+      if (startTime != null) {
+            ref.push({
+              start: startTime.getISOString(),
+              end: endTime.getISOString()
+            })
+          }
+          console.log(motionType + " motion detected (" + secondsDiff + " seconds)")
+      })
+    })
